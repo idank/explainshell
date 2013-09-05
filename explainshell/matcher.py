75a080199f463bc93d9341335a261a3d150333cf
Idan Kamara <idankk86@gmail.com>
2013-09-06 00:01:57 +0300

    matcher: integrate the new parser (only visits CommandNodes for now)

--- explainshell/matcher.py
+++ explainshell/matcher.py
@@ -1,5 +1,5 @@
 import collections, logging
-from explainshell import options, errors, util
+from explainshell import options, errors, util, parser
 
 class matchresult(collections.namedtuple('matchresult', 'start end text match')):
     @property
@@ -8,7 +8,7 @@ class matchresult(collections.namedtuple('matchresult', 'start end text match'))
 
 logger = logging.getLogger(__name__)
 
-class matcher(object):
+class matcher(parser.NodeVisitor):
     '''parse a command line and return a list of matchresults describing
     each token.
 
@@ -19,8 +19,8 @@ class matcher(object):
         self.section = section
         self.store = store
         self.manpage = None
-        self.pos = 0
         self._prevoption = self._currentoption = None
+        self.matches = []
 
     def find_option(self, opt):
         self._currentoption = self.manpage.find_option(opt)
@@ -33,46 +33,38 @@ class matcher(object):
         logger.info('found %r in store, got: %r, using %r', prog, manpages, manpages[0])
         return manpages
 
-    def unknown(self, token, endpos=None):
-        if endpos is None:
-            endpos = self.ts.endpos
+    def unknown(self, token, start, end):
         logger.debug('nothing to do with token %r', token)
-        return matchresult(self.pos, endpos, None, None)
+        return matchresult(start, end, None, None)
 
-    def nexttoken(self):
-        self.ts = self.tokens.next()
-        return self.ts
+    def visitcommand(self, node, parts):
+        assert parts
+        wordnode = parts.pop(0)
+        self.mps = self.findmanpages(wordnode.word)
+        self.manpage = self.mps[0]
+        endpos = wordnode.pos[1]
+        nextwordnode = parser.findfirstkind(parts, 'word')
 
-    def match(self):
-        '''parse s and return a list of matchresult
+        if self.manpage.multicommand and nextwordnode:
+            try:
+                multi = '%s %s' % (wordnode.word, nextwordnode.word)
+                logger.info('%r is a multicommand, trying to get another token and look up %r', self.manpage, multi)
+                self.mps = self.findmanpages(multi)
+                self.manpage = self.mps[0]
+                idx = 0 # parts.index(nextwordnode)
+                for p in parts:
+                    if p is nextwordnode:
+                        break
+                    idx += 1
+                assert idx < len(parts)
+                parts.pop(idx)
+                endpos = nextwordnode.pos[1]
+            except errors.ProgramDoesNotExist:
+                logger.info('no manpage %r for multicommand %r', multi, self.manpage)
+        self.matches.append(matchresult(node.pos[0], endpos, self.manpage.synopsis, None))
 
-        match works as follows:
-        - tokenize the string using options.tokenize
-        - look up a man page that matches the first token
-        - check if the man page has 'subcommands', e.g. git commit, if so
-          try to find a man page for the first two tokens
-        - iterate all tokens
-          - search the token as is
-          - if the token is a short option (-abc) try to look up each option
-            individually
-          - if the previous match expected an arg, merge this token with
-            the previous one
-          - partialmatch: if this man page allows options to start without '-',
-            try to match all characters individually (e.g. tar xzvf)
-          - check if this man page has any positional arguments
-          - mark this token as unknown
-
-        after we finish processing all tokens, we:
-        - merge unknown consecutive matches to become one matchresult
-        - merge adjacent matchresults that have the same help text (e.g. if we
-          had -vvv)
-        '''
-        logger.info('matching string %r', self.s)
-        self.tokens = util.peekable(options.tokenize(self.s))
-        #logger.info('tokenized %r to %s', self.s, [t[1] for t in self.tokens])
-        self.ts = None
-
-        def attempt_fuzzy(chars):
+    def visitword(self, node, word):
+        def attemptfuzzy(chars):
             m = []
             if chars[0] == '-':
                 tokens = [chars[0:2]] + list(chars[2:])
@@ -81,112 +73,97 @@ class matcher(object):
                 tokens = list(chars)
                 considerarg = False
 
-            oldp = self.pos
+            pos = node.pos[0]
             for t in tokens:
                 op = t if t[0] == '-' else '-' + t
                 option = self.find_option(op)
                 if option:
                     if considerarg and not m and option.expectsarg:
                         logger.info('option %r expected an arg, taking the rest too', option)
-                        return [matchresult(self.pos, self.pos+len(chars), option.text, None)]
+                        return [matchresult(pos, pos+len(chars), option.text, None)]
 
-                    mr = matchresult(self.pos, self.pos+len(t), option.text, None)
+                    mr = matchresult(pos, pos+len(t), option.text, None)
                     m.append(mr)
                 else:
-                    m.append(self.unknown(t, self.pos+len(t)))
-                self.pos += len(t)
-            self.pos = oldp
+                    m.append(self.unknown(t, pos, pos+len(t)))
+                pos += len(t)
             return m
 
-        self.pos, endpos, token = self.nexttoken()
-        mps = self.findmanpages(token)
-        self.manpage = mps[0]
-        if self.manpage.multicommand and self.tokens.hasnext():
-            try:
-                multi = '%s %s' % (token, self.tokens.peek()[2])
-                logger.info('%r is a multicommand, trying to get another token and look up %r', self.manpage, multi)
-                mps = self.findmanpages(multi)
-                self.manpage = mps[0]
-                self.nexttoken()
-                token = multi
-            except errors.ProgramDoesNotExist:
-                logger.info('no manpage %r for multicommand %r', multi, self.manpage)
+        logger.info('trying to match token: %r', word)
 
-        option = None
-        matches = []
-        matches.append(matchresult(0, len(token), self.manpage.synopsis, None))
-
-        while self.tokens.hasnext():
-            self.pos, endpos, token = self.nexttoken()
-            logger.info('trying to match token: %r', token)
-
-            self._prevoption = self._currentoption
-            option = self.find_option(token)
-            if option:
-                logger.info('found an exact match for %r: %r', token, option)
-                mr = matchresult(self.pos, self.ts.endpos, option.text, None)
-                matches.append(mr)
-            else:
-                if token != '-' and token.startswith('-') and not token.startswith('--'):
-                    logger.debug('looks like a short option')
-                    if len(token) > 2:
-                        logger.info("trying to split it up")
-                        matches.extend(attempt_fuzzy(token))
-                        self.pos += len(token)
-                    else:
-                        matches.append(self.unknown(token))
-                elif self._prevoption and self._prevoption.expectsarg:
-                    logger.info("previous option possibly expected an arg, and we can't"
-                            " find an option to match the current token, assuming it's an arg")
-                    ea = self._prevoption.expectsarg
-                    possibleargs = ea if isinstance(ea, list) else []
-                    take = True
-                    if possibleargs and token not in possibleargs:
-                        take = False
-                        logger.info('token %r not in list of possible args %r for %r',
-                                    token, possibleargs, self._prevoption)
-                    if take:
-                        pmr = matches[-1]
-                        mr = matchresult(pmr.start, self.ts.endpos, pmr.text, None)
-                        matches[-1] = mr
-                    else:
-                        matches.append(self.unknown(token))
-                elif self.manpage.partialmatch:
-                    logger.info('attemping to do a partial match')
-
-                    m = attempt_fuzzy(token)
-                    if any(mm.unknown for mm in m):
-                        logger.info('one of %r was unknown', token)
-                        matches.append(self.unknown(token))
-                    else:
-                        matches += m
-                        self.pos += len(token)
-                elif self.manpage.arguments:
-                    d = self.manpage.arguments
-                    k = list(d.keys())[0]
-                    logger.info('got arguments, using %r', k)
-                    text = d[k]
-                    mr = matchresult(self.pos, self.ts.endpos, text, None)
-                    matches.append(mr)
+        self._prevoption = self._currentoption
+        if word.startswith('--'):
+            word = word.split('=', 1)[0]
+        option = self.find_option(word)
+        if option:
+            logger.info('found an exact match for %r: %r', word, option)
+            mr = matchresult(node.pos[0], node.pos[1], option.text, None)
+            self.matches.append(mr)
+        else:
+            word = node.word
+            if word != '-' and word.startswith('-') and not word.startswith('--'):
+                logger.debug('looks like a short option')
+                if len(word) > 2:
+                    logger.info("trying to split it up")
+                    self.matches.extend(attemptfuzzy(word))
                 else:
-                    matches.append(self.unknown(token))
+                    self.matches.append(self.unknown(word, node.pos[0], node.pos[1]))
+            elif self._prevoption and self._prevoption.expectsarg:
+                logger.info("previous option possibly expected an arg, and we can't"
+                        " find an option to match the current token, assuming it's an arg")
+                ea = self._prevoption.expectsarg
+                possibleargs = ea if isinstance(ea, list) else []
+                take = True
+                if possibleargs and word not in possibleargs:
+                    take = False
+                    logger.info('token %r not in list of possible args %r for %r',
+                                word, possibleargs, self._prevoption)
+                if take:
+                    pmr = self.matches[-1]
+                    mr = matchresult(pmr.start, node.pos[1], pmr.text, None)
+                    self.matches[-1] = mr
+                else:
+                    self.matches.append(self.unknown(word, node.pos[0], node.pos[1]))
+            elif self.manpage.partialmatch:
+                logger.info('attemping to do a partial match')
+
+                m = attemptfuzzy(word)
+                if any(mm.unknown for mm in m):
+                    logger.info('one of %r was unknown', word)
+                    self.matches.append(self.unknown(word, node.pos[0], node.pos[1]))
+                else:
+                    self.matches += m
+            elif self.manpage.arguments:
+                d = self.manpage.arguments
+                k = list(d.keys())[0]
+                logger.info('got arguments, using %r', k)
+                text = d[k]
+                mr = matchresult(node.pos[0], node.pos[1], text, None)
+                self.matches.append(mr)
+            else:
+                self.matches.append(self.unknown(word, node.pos[0], node.pos[1]))
+
+    def match(self):
+        logger.info('matching string %r', self.s)
+        self.ast = parser.parse_command_line(self.s)
+        self.visit(self.ast)
 
         def debugmatch():
-            s = '\n'.join(['%d) %r = %r' % (i, self.s[m.start:m.end], m.text) for i, m in enumerate(matches)])
+            s = '\n'.join(['%d) %r = %r' % (i, self.s[m.start:m.end], m.text) for i, m in enumerate(self.matches)])
             return s
 
         logger.debug('%r matches:\n%s', self.s, debugmatch())
 
-        matches = self._mergeunknowns(matches)
-        matches = self._mergeadjacent(matches)
+        self.matches = self._mergeunknowns(self.matches)
+        self.matches = self._mergeadjacent(self.matches)
 
         # add matchresult.match to existing matches
-        for i, m in enumerate(matches):
+        for i, m in enumerate(self.matches):
             assert m.end <= len(self.s), '%d %d' % (m.end, len(self.s))
-            matches[i] = matchresult(m.start, m.end, m.text, self.s[m.start:m.end])
+            self.matches[i] = matchresult(m.start, m.end, m.text, self.s[m.start:m.end])
 
-        r = [(self.manpage.name, matches)]
-        for mp in mps[1:]:
+        r = [(self.manpage.name, self.matches)]
+        for mp in self.mps[1:]:
             r.append((mp, None))
         return r
 
