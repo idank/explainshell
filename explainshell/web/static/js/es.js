2c8e6cf8bcb2baa9f61ae754e29c88ca7c20848b
Idan Kamara <idankk86@gmail.com>
2013-09-15 11:45:18 +0300

    es.js: remove unused function

--- explainshell/web/static/js/es.js
+++ explainshell/web/static/js/es.js
@@ -1,9 +1,5 @@
 jQuery.fn.reverse = [].reverse;
 
-function randomcolor() {
-	return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'
-}
-
 var colors = ['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9'];
 
 var shuffledcolors;
