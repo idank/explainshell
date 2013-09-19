264d27157632a057bddab02c9fc8d61d47cc6c76
Idan Kamara <idankk86@gmail.com>
2013-09-19 20:57:43 +0300

    es.js: simple hack to disable console.log messages

--- explainshell/web/static/js/es.js
+++ explainshell/web/static/js/es.js
@@ -1,5 +1,12 @@
 jQuery.fn.reverse = [].reverse;
 
+var debug = false;
+
+if (!debug) {
+    console = console || {};
+    console.log = function(){};
+}
+
 // a list of colors to use for the lines
 var colors = ['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9'];
 
