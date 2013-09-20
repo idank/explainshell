fbd160ad96c16d47ae9750d5a78ed5a1e601aa6d
Idan Kamara <idankk86@gmail.com>
2013-09-20 14:26:15 +0300

    errors: change ParsingError message format
    Don't include the position pointer in the message, we'll do it manually where
    necessary.

--- explainshell/errors.py
+++ explainshell/errors.py
@@ -6,16 +6,9 @@ class EmptyManpage(Exception):
 
 class ParsingError(Exception):
     def __init__(self, message, s, position):
-        self.origmessage = message
+        self.message = message
         self.s = s
         self.position = position
 
         assert position <= len(s)
-
-        prefix = '%s (position %d, ' % (message, position)
-        indent = len(prefix)
-        if position - 5 > 0:
-            indent -= (position - 5)
-        indent += position
-        message = prefix + '%s)\n%s^' % (s[max(0, position - 5):], (indent * ' '))
-        super(ParsingError, self).__init__(message)
+        super(ParsingError, self).__init__('%s (position %d)' % (message, position))
