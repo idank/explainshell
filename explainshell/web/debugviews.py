dc2effe1b8ba91cc4159f8071c6c64916ed7f612
Idan Kamara <idankk86@gmail.com>
2013-12-07 21:07:17 +0200

    views/debug: allow editing the nestedcommand field

--- explainshell/web/debugviews.py
+++ explainshell/web/debugviews.py
@@ -24,6 +24,15 @@ def debug():
     d['manpages'].sort(key=lambda d: d['name'].lower())
     return render_template('debug.html', d=d)
 
+def _convertvalue(value):
+    if isinstance(value, list):
+        return [s.strip() for s in value]
+    elif value.lower() == 'true':
+        return True
+    elif value:
+        return value.strip()
+    return False
+
 @app.route('/debug/tag/<source>', methods=['GET', 'POST'])
 def tag(source):
     mngr = manager.manager(config.MONGO_URI, 'explainshell', [], False, False)
@@ -40,31 +49,37 @@ def tag(source):
             section = d['section']
             short = [s.strip() for s in d['short']]
             long = [s.strip() for s in d['long']]
-            if isinstance(d['expectsarg'], list):
-                expectsarg = [s.strip() for s in d['expectsarg']]
-            elif d['expectsarg'].lower() == 'true':
-                expectsarg = True
-            elif d['expectsarg']:
-                expectsarg = d['expectsarg']
-            else:
-                expectsarg = False
+            expectsarg = _convertvalue(d['expectsarg'])
+            nestedcommand = _convertvalue(d['nestedcommand'])
+            if isinstance(nestedcommand, str):
+                nestedcommand = [nestedcommand]
+            elif nestedcommand is True:
+                logger.error('nestedcommand %r must be a string or list', nestedcommand)
+                abort(503)
             argument = d['argument']
             if not argument:
                 argument = None
             p = store.paragraph(idx, text, section, d['is_option'])
             if d['is_option'] and (short or long or argument):
-                p = store.option(p, short, long, expectsarg, argument)
+                p = store.option(p, short, long, expectsarg, argument, nestedcommand)
             mparagraphs.append(p)
 
+        if request.form.get('nestedcommand', '').lower() == 'true':
+            m.nestedcommand = True
+        else:
+            m.nestedcommand = False
         m = mngr.edit(m, mparagraphs)
         if m:
-            return redirect(url_for('explain', program=m.name))
+            return redirect(url_for('explain', cmd=m.name))
         else:
             abort(503)
     else:
         helpers.convertparagraphs(m)
         for p in m.paragraphs:
-            if isinstance(p, store.option) and isinstance(p.expectsarg, list):
-                p.expectsarg = ', '.join(p.expectsarg)
+            if isinstance(p, store.option):
+                if isinstance(p.expectsarg, list):
+                    p.expectsarg = ', '.join(p.expectsarg)
+                if isinstance(p.nestedcommand, list):
+                    p.nestedcommand = ', '.join(p.nestedcommand)
 
         return render_template('tagger.html', m=m)
