497ebebf6c3c78866eb0f183dbd22b5b10772de1
Henning Weller <111561130+mundanevision20@users.noreply.github.com>
2024-09-15 23:13:56 +0000

    fix: replace inbuilt logging with loguru based helper scripts

--- runserver.py
+++ runserver.py
@@ -1,12 +1,15 @@
-import logging.config
+import logging
 
 from explainshell import config
 from explainshell.web import app
+from explainshell.logger.logger_helper import logger
+from explainshell.logger.logging_interceptor import InterceptHandler
 
-
-logging.config.dictConfig(config.LOGGING_DICT)
 
 if __name__ == '__main__':
+    # activate logging and redirect all logs to loguru
+    logging.basicConfig(handlers=[InterceptHandler()], level=logging.DEBUG, force=True)
+
     if len(config.HOST_IP) > 1:
         app.run(debug=config.DEBUG, host=config.HOST_IP)
     else:
