423315a67044345f97d6599e87a9c17edcd4042b
Henning Weller <111561130+mundanevision20@users.noreply.github.com>
2024-09-15 23:13:56 +0000

    fix: replace inbuilt logging with loguru based helper scripts

--- explainshell/config.py
+++ explainshell/config.py
@@ -12,28 +12,3 @@ MAN2HTML = os.path.join(TOOLS_DIR, "w3mman2html.cgi")
 HOST_IP = os.getenv("HOST_IP", "")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost")
 DEBUG = True
-
-LOGGING_DICT = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
-    },
-    "handlers": {
-        "console": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
-        },
-        "file": {
-            "class": "logging.FileHandler",
-            "level": "INFO",
-            "formatter": "standard",
-            "filename": "application.log",
-            "mode": "a",
-        },
-    },
-    "loggers": {
-        "explainshell": {"handlers": ["console"], "level": "INFO", "propagate": False}
-    },
-}
