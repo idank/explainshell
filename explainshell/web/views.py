7b46fdd1394be41e4c5462401ae55dbd457b4e3a
Idan Kamara <i@idank.me>
2026-03-08 12:54:52 +0200

    chore: remove about page and showthedocs banner

--- explainshell/web/views.py
+++ explainshell/web/views.py
@@ -96,10 +96,6 @@ def index():
     return render_template("index.html")
 
 
-@bp.route("/about")
-def about():
-    return render_template("about.html")
-
 
 @bp.route("/explain", defaults={"path": ""})
 @bp.route("/explain/<path:path>")
