33e01cd4965aeb10c2e7556e19ad7b12e0549428
Idan Kamara <i@idank.me>
2026-04-14 16:56:14 +0300

    feat(deploy): add gunicorn access log with request duration

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
 
-exec gunicorn -w 2 --threads 4 -b 0.0.0.0:8080 "explainshell.web:create_app()"
+exec gunicorn -w 2 --threads 4 -b 0.0.0.0:8080 \
+  --access-logfile - \
+  --access-logformat '%(t)s "%(r)s" %(s)s %(b)s %(D)sμs' \
+  "explainshell.web:create_app()"
